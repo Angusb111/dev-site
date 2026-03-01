3610635f07e33ba56e00ba9b85cbd12acbbdaddd
Angus Bodle <bodleangus@gmail.com>
2026-03-01 14:41:18 +1300

    Verison1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Projects() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -16,20 +16,13 @@ export default function Home() {
             Angus Bodle
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Web Developer -{" "}
-            <a
-              href="https://photo.angusbodle.com"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Photographer
-            </a>{" "}
-            - Trying
+            Projects
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="./projects"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
