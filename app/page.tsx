d1eb12ef186797339d97198d1830b68206641d53
Angusb111 <bodleangus@gmail.com>
2026-03-17 01:19:50 +1300

    Filling out Projects Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black sm:items-start">
         <Image
           src="/logo.png"
           alt="My Logo"
@@ -12,14 +13,14 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xl text-7xl font-semibold leading-15 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xl text-7xl font-semibold leading-15 tracking-tight text-zinc-50">
             Angus Bodle
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400">
             Web Developer -{" "}
             <a
               href="https://photo.angusbodle.com"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="font-medium text-zinc-50"
             >
               Photographer
             </a>{" "}
@@ -28,9 +29,9 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            className="flex h-10 w-[fit-content] p-2 w-full items-center justify-around gap-2 rounded-sm bg-[#011818] border border-teal-300 text-teal-300 font-[consolas] transition-colors hover:bg-teal-950 dark:hover:bg-teal-950 md:w-[158px]"
             href="./projects"
-            target="_blank"
+            target="_self"
             rel="noopener noreferrer"
           >
             <Image
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Projects() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black sm:items-start">
         <Image
           src="/logo.png"
           alt="My Logo"
@@ -12,10 +12,47 @@ export default function Projects() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-s text-7xl font-semibold leading-15 tracking-tight text-black dark:text-zinc-50">
-            AT Some Point, there will be a list of projects here
-          </h1>
-          <a href="./pong.html" className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">Play Pong</a>
+
+
+          <div className="flex flex-col gap-2 pb-3 bg-[#111] shadow-none border border-teal-300 rounded-sm hover:shadow-[0px_0px_10px_#46edd5] transition-shadow max-w-xs text-white text-sm">
+            <div className="flex flex-row gap-20 p-3 justify-between items-center"> 
+              <p className="text-white text-2xl font-semibold">Auto Tyres</p>
+              <div className="flex flex-row border border-teal-300 text-teal-300 rounded-sm items-center h-5">
+                <p className="text-xs leading-none p-1 ps-2">Live</p>
+                <div className="border border-teal-300 rounded-full h-2 w-2 m-2 ms-1"></div>
+              </div>
+            </div>
+            <img src="https://autotyresgisborne.co.nz/images/220/14239563/AUTO_TYRES_COLOR.webp" className="p-4" alt="" />
+            <p className="px-3">
+              Branding Photography and Static Website Design. Business has changed hands since and original design no longer in use.
+            </p>
+            <p className="ps-3 pt-2">Technology Used:</p>
+            <p className="ps-3">
+              <ul className="list-disc ps-5">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Web Hosting</li>
+                <li>Photography</li>
+              </ul>
+            </p>
+          </div>
+
+
+          <a
+            className="flex h-10 w-[fit-content] p-2 w-full items-center justify-around gap-2 rounded-sm bg-[#011818] border border-teal-300 text-teal-300 font-[consolas] transition-colors hover:bg-teal-950 dark:hover:bg-teal-950 md:w-[158px]"
+            href="https://angusbodle.com"
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/cog.svg"
+              alt="wrench icon"
+              width={20}
+              height={20}
+            />
+            This Website
+          </a>
+          <a href="./pong.html" className="max-w-md text-lg leading-8 text-zinc-400">Play Pong</a>
         </div>
       </main>
     </div>
